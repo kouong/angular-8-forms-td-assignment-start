08322ad9b6777cf57b39458febc1b05256a09bce
kouong <gerkoford85@gmail.com>
2019-11-14 22:09:05 +0100

    angular 8 form template driven assignment finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f', {static: false}) signupForm: NgForm;
+  defaultSubscription = 'advanced';
+  submitted = false;
+
+  user = {
+    email: '',
+    subscription: '',
+    password: ''
+  };
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  populate() {
+    // this.signupForm.setValue({
+    //   email: 'test@test.com',
+    //   subscription: 'basic',
+    //   password: 'pass'
+    // });
+
+    this.signupForm.form.patchValue({
+      email: 'test@test.com',
+      subscription: 'basic'
+    });
+  }
+
+  onSubmit() {
+    // console.log(this.signupForm);
+        this.submitted = true;
+        this.user.email = this.signupForm.value.email;
+        this.user.subscription = this.signupForm.value.subscription;
+        this.user.password = this.signupForm.value.password;
+        this.signupForm.reset();
+  }
+
 
 }
